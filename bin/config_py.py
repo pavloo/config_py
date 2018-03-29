d14643d6b6320359ef1cbe0b8e78710037b48deb
Pavlo Osadchyi <posadchiy@gmail.com>
2018-03-29 13:54:19 +0300

    close opened files; refactor to get rid from string concatenation in building paths

--- bin/config_py.py
+++ bin/config_py.py
@@ -13,7 +13,7 @@ MODULE_SRC_DIR = os.path.dirname(os.path.realpath(__file__)) + '/../fixtures/mod
 
 def generate_root_config():
     src_dir = ROOT_SRC_DIR
-    conf_dir = os.getcwd() + '/' + CONF_DIR_NAME
+    conf_dir = os.path.join(os.getcwd(), CONF_DIR_NAME)
     if not os.path.exists(conf_dir):
         os.makedirs(conf_dir)
         shutil.copy2(src_dir + '__init__.py', conf_dir)
@@ -29,7 +29,7 @@ def generate_root_config():
 
 def generate_module_config(module):
     src_dir = MODULE_SRC_DIR
-    conf_dir = os.getcwd() + '/' + '/'.join(module.split('.')) + '/' + CONF_DIR_NAME + '/'
+    conf_dir = os.path.join(os.getcwd(), *module.split('.'), CONF_DIR_NAME)
     if os.path.exists(conf_dir):
         click.secho(
             ERROR_ALREADY_EXISTS_FMT.format('"{}"'.format(module)),
@@ -39,14 +39,15 @@ def generate_module_config(module):
         return
 
     os.makedirs(conf_dir)
-    src_init_fmt = open(src_dir + '__init__.py').read()
-    dest_init_f = open(conf_dir + '__init__.py', 'w+')
-    dest_init_f.write(src_init_fmt.format(module=module))
+    with open(os.path.join(src_dir, '__init__.py'), 'r') as src_init, \
+         open(os.path.join(conf_dir, '__init__.py'), 'w+') as dest_init_f:
+        src_init_fmt = src_init.read()
+        dest_init_f.write(src_init_fmt.format(module=module))
     shutil.copy2(src_dir + DEV_FILE, conf_dir)
 
 
 @click.command()
-@click.option("-m", "--module", help="module to create a config for", default=None)
+@click.option("-m", "--module", help="python module to create a config for", default=None)
 def config_py(module):
     if not module:
         generate_root_config()
